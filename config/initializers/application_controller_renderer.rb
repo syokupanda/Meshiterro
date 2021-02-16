67a78a92eae1dc47143bf362993f66e7428e5924
syokupanda <borun.books@gmail.com>
2021-02-16 05:09:59 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8beb869338647b5cdc768aaad60077e06ce47c52dc68a75c8f4d36b6ec2829a779767ca3d9e3640f6ec2ad91893dabf0ce58d7e450187140e9867a90b845fd14'
